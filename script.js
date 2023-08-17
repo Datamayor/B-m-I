a7ac8b70b57b67d041e8b37a3b15a18ba7888170
Datamayor <106524048+Datamayor@users.noreply.github.com>
2023-08-17 13:30:30 +0100

    cooking

---
+++ script.js
@@ -0,0 +1,15 @@
+// //Mail Script
+// function SendMail(){
+// 	var params = {
+// 		last_name : document.getElementById("last_name").value,
+//         phone_number : document.getElementById("phone_number").value,
+//         address : document.getElementById("address").value,
+//         choice : document.getElementById("choice").value,
+// 		from_name : document.getElementById("f_name").value,
+// 		email_id : document.getElementById("email_id").value,
+// 		message : document.getElementById("message").value,
+// 	}
+// 	emailjs.send("service_77ebd5b", "template_vu5nbyn", params).then(function (res){
+// 		alert("Success! " + res.status);
+// 	})
+// }

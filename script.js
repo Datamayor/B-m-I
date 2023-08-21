ae5102011ebc2f79b4c52048527bf0292aa699b1
Datamayor <106524048+Datamayor@users.noreply.github.com>
2023-08-21 18:06:53 +0100

    cooking

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-// //Mail Script
-// function SendMail(){
-// 	var params = {
-// 		last_name : document.getElementById("last_name").value,
-//         phone_number : document.getElementById("phone_number").value,
-//         address : document.getElementById("address").value,
-//         choice : document.getElementById("choice").value,
-// 		from_name : document.getElementById("f_name").value,
-// 		email_id : document.getElementById("email_id").value,
-// 		message : document.getElementById("message").value,
-// 	}
-// 	emailjs.send("service_77ebd5b", "template_vu5nbyn", params).then(function (res){
-// 		alert("Success! " + res.status);
-// 	})
-// }
+let button = document.getElementById('btn');
+
+button.addEventListener('click', () => {
+  const height = parseInt(document.getElementById('height').value);
+  const weight = parseInt(document.getElementById('weight').value);
+  const result = document.getElementById('output');
+  let height_status = false,
+    weight_status = false;
+
+  if (height === '' || isNaN(height) || height <= 0) { 
+    document.getElementById('height_error').innerHTML =
+      'please provide a valid height';
+  } else {
+    document.getElementById('height_error').innerHTML = '';
+    height_status = true;
+  }
+
+  if (weight === '' || isNaN(weight) || weight <= 0) {
+    document.getElementById('weight_error').innerHTML =
+      'please provide a valid weight';
+  } else {
+    document.getElementById('weight_error').innerHTML = '';
+    weight_status = true;
+  }
+
+
+if (height_status && weight_status) {
+  const bmi = (weight / ((height * height)/1000)).toFixed(2);
+
+  if (bmi > 24.29) {
+    result.innerHTML = 'Your BMI is ' + bmi + ', so you are overweight.';
+  } else if (bmi > 18.5 && bmi < 24.5) {
+    result.innerHTML = 'Your BMI is ' + bmi + ', so you have a normal weight.';
+  } else {
+    result.innerHTML = 'Your BMI is ' + bmi + ', so you are underweight.';
+  }
+}
+  else{
+    alert('the form has errors');
+    result.innerHTML = '';
+  }
+});
+
